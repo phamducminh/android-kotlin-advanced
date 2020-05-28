b9411f732cb3ef48c9b6e42d3e7ba1e56cf8cb51
Minh Pham <pdminh0410@gmail.com>
2020-05-28 22:29:09 +0700

    Advanced Android in Kotlin 01.2: Android Firebase Cloud Messaging - 5. Task: Starting with Firebase Cloud Messaging

--- android-kotlin-notifications-fcm-start/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ android-kotlin-notifications-fcm-start/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -31,9 +31,9 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
     // [START receive_message]
-    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: ${remoteMessage?.from}")
+        Log.d(TAG, "From: ${remoteMessage.from}")
 
         // TODO Step 3.5 check messages for data
         // Check if message contains a data payload.
@@ -47,6 +47,20 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     //TODO Step 3.2 log registration token
     // [START on_new_token]
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the
+     * InstanceID token is initially generated so this is where you would retrieve
+     * the token.
+     */
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token)
+    }
 
     // [END on_new_token]
 
--- android-kotlin-notifications-fcm-start/app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ android-kotlin-notifications-fcm-start/app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -57,8 +57,13 @@ class EggTimerFragment : Fragment() {
         )
 
         // TODO: Step 3.1 create a new channel for FCM
+        createChannel(
+            getString(R.string.breakfast_notification_channel_id),
+            getString(R.string.breakfast_notification_channel_name)
+        )
 
         // TODO: Step 3.4 call subscribe topics on start
+        subscribeTopic()
 
         return binding.root
     }
@@ -93,6 +98,18 @@ class EggTimerFragment : Fragment() {
     }
 
     // TODO: Step 3.3 subscribe to breakfast topic
+    private fun subscribeTopic() {
+        // [START subscribe_topics]
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+            .addOnCompleteListener { task ->
+                var msg = getString(R.string.message_subscribed)
+                if (!task.isSuccessful) {
+                    msg = getString(R.string.message_subscribe_failed)
+                }
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+            }
+        // [END subscribe_topics]
+    }
 
     companion object {
         fun newInstance() = EggTimerFragment()
