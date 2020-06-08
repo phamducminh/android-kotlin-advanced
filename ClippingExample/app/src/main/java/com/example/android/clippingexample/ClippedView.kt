211c4f8c5d4b49630919138e32af130f9ef45764
Minh Pham <pdminh0410@gmail.com>
2020-06-08 12:39:08 +0700

    Advanced Android in Kotlin 02.3: Clipping Canvas Objects - 4. Task: Understanding the drawing algorithm

--- ClippingExample/app/src/main/java/com/example/android/clippingexample/ClippedView.kt
+++ ClippingExample/app/src/main/java/com/example/android/clippingexample/ClippedView.kt
@@ -1,6 +1,7 @@
 package com.example.android.clippingexample
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
@@ -41,4 +42,48 @@ class ClippedView @JvmOverloads constructor(
     private val rowThree = rowTwo + rectInset + clipRectBottom
     private val rowFour = rowThree + rectInset + clipRectBottom
     private val textRow = rowFour + (1.5f * clipRectBottom)
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        drawBackAndUnclippedRectangle(canvas)
+        drawDifferenceClippingExample(canvas)
+        drawCircularClippingExample(canvas)
+        drawIntersectionClippingExample(canvas)
+        drawCombinedClippingExample(canvas)
+        drawRoundedRectangleClippingExample(canvas)
+        drawOutsideClippingExample(canvas)
+        drawSkewedTextExample(canvas)
+        drawTranslatedTextExample(canvas)
+        // drawQuickRejectExample(canvas)
+    }
+
+    private fun drawBackAndUnclippedRectangle(canvas: Canvas) {
+    }
+
+    private fun drawDifferenceClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawCircularClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawIntersectionClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawCombinedClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawRoundedRectangleClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawOutsideClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawTranslatedTextExample(canvas: Canvas) {
+    }
+
+    private fun drawSkewedTextExample(canvas: Canvas) {
+    }
+
+    private fun drawQuickRejectExample(canvas: Canvas) {
+    }
 }
